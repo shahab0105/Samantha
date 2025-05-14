e570fc0f1639cc3e254ae1c6b284d44ff0f90c04
shahabuddinirfan@gmail.com <shahabuddinirfan@gmail.com>
2025-05-14 20:04:03 +0500

    text chunking logic

---
+++ src/document/loader.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+function loadAndChunkDocs(
+  dir: string,
+  chunkSize: number = 300
+): { id: string; text: string }[] {
+  const files = fs.readdirSync(dir);
+  const allChunks: { id: string; text: string }[] = [];
+  for (const file in files) {
+    const filePath = path.join(dir, file);
+    const content = fs.readFileSync(filePath, "utf-8");
+    const chunks = chunkText(content, chunkSize);
+    chunks.forEach((ch, i) => {
+      allChunks.push({
+        id: `${file}::part-${i}`,
+        text: ch,
+      });
+    });
+  }
+  return allChunks;
+}
+
+function chunkText(text: string, maxWords: number = 300): string[] {
+  const words = text.split(/\s+/);
+  const chunks: string[] = [];
+  for (let i = 0; i < words.length; i += maxWords) {
+    const chunk = words.slice(i, i + maxWords).join(" ");
+    chunks.push(chunk);
+  }
+  return chunks;
+}
---
+++ src/utils/embeddingUtils.ts
@@ -0,0 +1,6 @@
+// so I'll create embedding/sementics with openais embedding model
+import { OpenAI } from "openai";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
